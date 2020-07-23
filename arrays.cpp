df20f8ba3a87503629afc895dd368f9294d9ae7d
harishmith-soundaranayaki <68113285+harishmith-soundaranayaki@users.noreply.github.com>
2020-07-23 19:34:58 +0530

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+   int a[5],i;
+   cout<<"Enter elements:\n";
+   for(i=0; i<5; i++)
+   cin>>a[i];
+   cout<<"you entered:\n ";
+   for(i=0; i<5; i++)
+   cout<<a[i]<<"\n ";
+   return 0;
+}
